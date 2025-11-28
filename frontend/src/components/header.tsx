0c75b281dc3ac88a125280f0f2a483bcdce34811
ericksax <erickfreitas@gmail.com>
2025-11-27 23:51:10 -0300

    refactor: restructure application layout and integrate Tailwind CSS

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="p-8 fixed top-0 left-0 w-full flex items-center justify-between">
+      <div className="flex items-center space-x-4  bg-[#1A1D23] p-8 rounded-2xl  w-full h-24">
+        <h1 className="text-emerald-600 text-3xl font-bold">Dash.Weather</h1>
+      </div>
+    </header>
+  );
+}
